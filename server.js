ea3a583ae6f40d9fdb4b09d03c63c71c242d2d6a
unknown <alexalister1919@gmail.com>
2023-06-26 02:36:43 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express()
+
+app.use('/', (req, res) => {
+    res.json({
+        "name": "Piyush",
+        "age": 21,
+        "email": "piyush@mailinator.com",
+        "role": "SDE | Backend",
+        "Company": "Cutshort"
+    })
+})
+
+app.listen(8080, () => {
+    console.log('Server listening on PORT 8080...')
+})
